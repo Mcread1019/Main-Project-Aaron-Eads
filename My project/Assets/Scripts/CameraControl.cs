a509a5da76afe52cff61c291269a63ac9f05a4d7
Mcread1019 <143539294+Mcread1019@users.noreply.github.com>
2023-10-23 18:46:57 -0400

    Finished Camera controls

--- My project/Assets/Scripts/CameraControl.cs
+++ My project/Assets/Scripts/CameraControl.cs
@@ -4,17 +4,29 @@ using UnityEngine;
 
 public class CameraControl : MonoBehaviour
 {
-    [SerializeField] GameObject povCam;
+   
     public CharacterController controller;
+    public Transform cam;
     public float speed = 6f;
+
+    public float SmoothTurnTime = 0.1f;
+    float SmoothTurnVelocity;
 
     // Update is called once per frame
     void Update()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vectical");
+        float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction =new Vector3(horizontal, 0f,vertical).normalized;
 
-        if
+        if(direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x,direction.z)* Mathf.Rad2Deg+cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref SmoothTurnVelocity,SmoothTurnTime);
+            transform.rotation = Quaternion.Euler(0f,angle,0);
+
+            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDirection.normalized*speed* Time.deltaTime);
+        }
     }
 }
